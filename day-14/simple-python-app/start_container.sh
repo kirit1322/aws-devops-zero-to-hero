b2e917451299c5a7cd082975c43af558cc416cb7
kirit1322 <kiritpatel9082@gmail.com>
2026-03-03 15:13:07 +0530

    file update

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-echo
+echo "Stopping existing container (if running)..."
+docker stop simple-flask-app || true
+docker rm simple-flask-app || true
 
-# Run the Docker image as a container
-echo
+echo "Pulling latest Docker image..."
+docker pull kiritpatel1321/simple-python-flask-app
+
+echo "Running new container..."
+docker run -d -p 5000:5000 --name simple-flask-app kiritpatel1321/simple-python-flask-app
+
+echo "Deployment completed successfully!"
